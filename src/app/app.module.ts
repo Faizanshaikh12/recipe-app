f0af26f99829d1913946f7cd8c978b8207a57f73
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-03-27 18:56:38 +0530

    Create All Projects Components....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes-list/recipe-item/recipe-item.component';
+import { ShoppinglListComponent } from './shoppingl-list/shoppingl-list.component';
+import { ShoppingEditComponent } from './shoppingl-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppinglListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
